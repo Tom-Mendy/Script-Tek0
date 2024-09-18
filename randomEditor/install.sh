55b96fc9514da7b3b40d76e83f3d6495854d76b5
Tom Mendy <tom.mendy@epitech.eu>
2024-09-18 15:04:52 +0200

    [FIX] randomEditor

--- randomEditor/install.sh
+++ randomEditor/install.sh
@@ -4,3 +4,5 @@ cp ~/.bashrc ~/.bashrc.bak
 echo "alias vim='rand=("vim" "emacs" "nano"); \${rand["\$(shuf -i 0-${#rand[@]} -n 1)"]}'" >> ~/.bashrc
 echo "alias emacs='rand=("vim" "emacs" "nano"); \${rand["\$(shuf -i 0-${#rand[@]} -n 1)"]}'" >> ~/.bashrc
 echo "alias nano='rand=("vim" "emacs" "nano"); \${rand["\$(shuf -i 0-${#rand[@]} -n 1)"]}'" >> ~/.bashrc
+
+source ~/.bashrc
